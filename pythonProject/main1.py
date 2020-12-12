c4bbe8bb26602a5a74d8fa2579b458cbfd2f40ab
kumlowhup <kumlowhup@yahoo.co.jp>
2020-12-12 22:07:23 +0800

    Initial commit

---
+++ pythonProject/main1.py
@@ -0,0 +1,39 @@
+# 先导入要用的一堆工具
+from urllib import request
+import json
+from urllib import parse
+import os, win32gui, win32con, win32api
+import datetime
+
+
+os.chdir('BingPictures')
+datetoday = datetime.date.today()
+print(datetoday.strftime("今天是20%y年%m月%d日"))
+# 运行时间有点久就让操作者等待
+print('壁纸下载中，请稍后。。')
+
+# 先找到必应每日一图对应的js文件的网址
+sourceWebsite = request.urlopen("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1")
+obj = json.load(sourceWebsite)
+
+# 在文件信息里打印出'copyright',即版权信息中包含的图片名字
+print('今天的图片是：')
+print(obj['images'][0]['copyright'])
+
+# (obj['images'][0]['urlbase'])提取每日的照片的网址部分使用
+photoURL = 'http://www.bing.com' + (obj['images'][0]['urlbase']) + '_1920x1080.jpg'
+f = open('./wallpaper.jpg', 'wb')
+picture = request.urlopen(photoURL)
+f.write(picture.read())
+f.close()
+
+# 提取js中的'copyrightlink'项(地名)
+nameurl = obj['images'][0]['copyrightlink']
+
+# 去头去尾，只剩下汉字搜索对应的url的字码
+photoname = nameurl.replace('https://www.bing.com/search?q=', '')
+photoname = photoname.replace('&form=hpcapt&mkt=zh-cn', '')
+# 把url字码解码，转回原来汉字
+photoname = parse.unquote(photoname)
+print(photoname)
+os.rename('wallpaper.jpg',datetoday.strftime('20%y%m%d') + photoname + '.jpg')
